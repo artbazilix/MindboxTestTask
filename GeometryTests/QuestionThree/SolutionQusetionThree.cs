5d0eb849f559059c704d9902c04f28a2fddfd21d
artbazilix <artbazilix@gmail.com>
2023-11-24 02:59:05 +0300

    update

---
+++ GeometryTests/QuestionThree/SolutionQusetionThree.cs
@@ -0,0 +1,19 @@
+﻿/* 
+
+Предположим, что есть две таблицы: Products и Categories 
+и они связаны через таблицу ProductCategories:
+
+SELECT Products.ProductName, Categories.CategoryName
+FROM Products
+LEFT JOIN ProductCategories ON Products.ProductID = ProductCategories.ProductID
+LEFT JOIN Categories ON ProductCategories.CategoryID = Categories.CategoryID;
+
+
+
+Если таблицы связаны напрямую:
+
+SELECT Products.ProductName, Categories.CategoryName
+FROM Products
+LEFT JOIN Categories ON Products.CategoryID = Categories.CategoryID;
+
+*/
